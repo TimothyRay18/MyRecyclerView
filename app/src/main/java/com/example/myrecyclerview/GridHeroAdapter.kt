ec6d24f8eb19e9a2dda6e9bf0e70c57823571b61
TimothyRay18 <timothy.ray1802@gmail.com>
2022-03-09 11:41:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.myrecyclerview
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.myrecyclerview.Hero
+import com.example.myrecyclerview.databinding.ItemGridHeroBinding
+
+class GridHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): GridViewHolder {
+        val binding = ItemGridHeroBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return GridViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        holder.bind(listHero[position])
+    }
+
+    inner class GridViewHolder ( private val binding : ItemGridHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(hero: Hero) {
+            with(binding){
+                Glide.with(itemView.context)
+                    .load(hero.photo)
+                    .apply(RequestOptions().override(350, 550))
+                    .into(imgItemPhoto)
+            }
+        }
+    }
+}
